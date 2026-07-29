91405d904f9cfad6f20cef1584f644a1b919b7e7
T Gowtham Jignas <gowthamjignas1029@gmail.com>
2026-07-29 16:34:35 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         x=len(s)
         left,right =0,x-1
-        for i in range len(s):
+        for i in range x:
             while left<right:
                 if s[left]!=s[right]:
                     return False
