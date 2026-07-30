85ae9418145f26f708bb6d67c4b1cef311a18e73
T Gowtham Jignas <gowthamjignas1029@gmail.com>
2026-07-30 16:26:17 +0530

    LC-maximum-average-subarray-i [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/maximum-average-subarray-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        left,right=0,3
+        sum,max=0,0
+        for i in range (k):
+            sum= sum+nums[i]
+            max=sum
+        for j in range(k,len(nums)):
+            sum = sum-(nums[left])+(nums[right+1])
+            left+=1
+            right+=1
+            if sum>max:
+                max=sum
+        return max/k
